a9d978121a83d42de126abe42f09556330c8d0a8
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-04-08 14:04:53 +0200

    Fix dependencies for gobject-introspection and Vapigen
    Some parts use gobject-introspection or vapigen, which require
    Vala and Gobject-introspection parts being built before.
    Unfortunately, some dependencies didn't account for this.
    
    Also, the PATH environment was wrong, and didn't prioritize the
    STAGE, so in some cases the wrong binary was being used.
    
    Due to all of this, building libportal failed when using
    gobject-introspection greater than 1.82.0.
    
    This PR fixes all these things, and also adds an extra check in
    check_dependencies.py to ensure that any part that is building
    gobject-introspection or vapi files have the correct
    dependencies in the `after` entry.

--- tools/check_dependencies.py
+++ tools/check_dependencies.py
@@ -4,15 +4,17 @@ import os
 import sys
 import yaml
 
-# Ensure that "deb" part depends on all previous parts
-
 config_file = "snapcraft.yaml"
 if not os.path.exists(config_file):
     config_file = os.path.join("snap", "snapcraft.yaml")
 
 data = yaml.safe_load(open(config_file, "r"))
 
-exceptions = ['buildenv']
+########################################################
+# Ensure that "deb" part depends on all previous parts #
+########################################################
+
+deb_exceptions = ['buildenv']
 
 def filldeps(part):
     """ Fill recursively the dependencies of each part """
@@ -38,7 +40,7 @@ for part in data["parts"]:
     if "debs" in dependencies[part]:
         # if it depends on debs, it must be after, so don't take it into account
         continue
-    if part in exceptions:
+    if part in deb_exceptions:
         continue
     deb_dependencies.append(part)
 
@@ -46,6 +48,48 @@ failed = False
 for dependency in deb_dependencies:
     if dependency not in dependencies["debs"]:
         print(f"DEBS part must be after {dependency}")
+        failed = True
+
+def check_dependency(*, part_name, part, string_to_find, dependency):
+    if "meson-parameters" not in part:
+        return False
+    for meson_parameter in part["meson-parameters"]:
+        if meson_parameter.find(string_to_find) == -1:
+            continue
+        if dependency not in part["after"]:
+            print(f"Part {part_name} requires {dependency} dependency due to meson option {meson_parameter}")
+            return True
+    return False
+
+
+###################################################
+# Ensure that any part that builds introspection, #
+# depends on gobject-introspection.               #
+###################################################
+
+goi_exceptions = ["glib"]
+for part_name in data["parts"]:
+    if part_name in goi_exceptions:
+        continue
+    if check_dependency(part_name=part_name,
+                        part = data["parts"][part_name],
+                        string_to_find="introspection",
+                        dependency="gobject-introspection"):
+        failed = True
+
+###############################################
+# Ensure that any part that builds VAPI info, #
+# depends on vala.                            #
+###############################################
+
+vapi_exceptions = []
+for part_name in data["parts"]:
+    if part_name in vapi_exceptions:
+        continue
+    if check_dependency(part_name=part_name,
+                        part = data["parts"][part_name],
+                        string_to_find="vapi",
+                        dependency="vala"):
         failed = True
 
 if failed:
